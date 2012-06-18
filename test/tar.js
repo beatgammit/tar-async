946c33e20b63bfec7573f77c0d7784a0aec1aa5a
T. Jameson Little <t.jameson.little@gmail.com>
2012-06-18 16:10:11 -0600

    Added super basic test suite

--- test/tar.js
+++ test/tar.js
@@ -3,20 +3,30 @@
 	'use strict';
 
 	var fs = require('fs'),
-		Tar = require('tar-async'), // or require('tar-async').Tar or require('tar-async/tar')
-		tape,
-		contents,
-		stats;
+		path = require('path'),
+		Tar = require('../lib'),
+		tape;
 
-	contents = fs.readFileSync('./tar.js', 'utf8');
+	function tarDir(dir, tape) {
+		var files = fs.readdirSync(dir);
 
-	tape = new Tar({output: require('fs').createWriteStream('out.tar')});
+		files.forEach(function (file) {
+			var fullPath = path.join(dir, file),
+				contents = fs.readFileSync(fullPath);
 
-	stats = fs.lstatSync('./tar.js');
+			tape.append(file, contents);
+		});
+	}
 
-	tape.append('waja.js', contents);
-	tape.append('piped.js', fs.createReadStream('./tar.js', {encoding: 'utf8'}), {size: stats.size});
-	tape.append('justText.txt', 'This is just some text. Fun, huh?', function () {
-		tape.close();
+	fs.readdirSync('.').forEach(function (file) {
+		var stats = fs.statSync(file);
+
+		if (stats.isDirectory()) {
+			console.log(file);
+
+			tape = new Tar({output: fs.createWriteStream(file + '.tar')});
+
+			tarDir(file, tape);
+		}
 	});
 }());
